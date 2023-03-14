01fca0ff420a8b4a22317c648aa687f4cc2b192e
Pengin <fierce0728birds@gmail.com>
2023-03-14 16:59:40 +0900

    ファイル追加

--- server/config/settings_local.py
+++
@@ -1,1 +0,0 @@
-SECRET_KEY = 'xz3diu6trv9=v_3=0rblrtm$i=knp-m^^bsd1hyp_k&4bo@ap&'
--- server/manage.py
+++ server/manage.py
@@ -7,10 +7,6 @@ import sys
 # シェルに入ったらこのコマンド打つ
 # pip install pymysql
 # apt-get -y install default-mysql-client
-# pip install djangorestframework djangorestframework-jwt
-# settings_local.py='djangoのシークレットキー追加'
-# pip install Pillow
-
 
 
 
