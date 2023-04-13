41f1a4ce9c977b89c8aff81b8242645a9cc55a69
Pengin <fierce0728birds@gmail.com>
2023-04-13 18:30:08 +0900

    tools追加

--- server/build_files.sh
+++ server/build_files.sh
@@ -3,6 +3,8 @@ yum update
 yum clean all
 yum install -y libpq-dev gcc
 yum -y install gcc libmariadb-dev mariadb-client
+python -m pip install -U pip
+pip install upgrade setuptools
 pip install -r requirements.txt
 pip install --upgrade pip
 pip install django-allauth
