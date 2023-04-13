75a83401906fb698a99adc31cc62dd017bfde2ec
Pengin <fierce0728birds@gmail.com>
2023-04-13 18:24:03 +0900

    yum

--- server/build_files.sh
+++ server/build_files.sh
@@ -4,5 +4,8 @@ yum clean
 yum install -y libpq-dev gcc
 yum -y install gcc libmariadb-dev mariadb-client
 pip install -r requirements.txt
+pip install --upgrade pip
+pip install django-allauth
+pip install django-widget-tweaks
 python3.9 manage.py collectstatic --noinput --clear
 echo "BUILD END"
