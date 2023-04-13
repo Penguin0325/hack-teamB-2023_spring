9452029f2a2d99b600bff1c9d8d94ba8dc1a60d1
Pengin <fierce0728birds@gmail.com>
2023-04-13 19:45:46 +0900

    tools追加

--- server/build_files.sh
+++ server/build_files.sh
@@ -2,6 +2,7 @@ echo "BUILD START"
 yum update
 yum clean all
 yum install -y libpq-dev gcc
+yum install --upgrade pip setuptools
 yum -y install gcc libmariadb-dev mariadb-client
 pip install -r requirements.txt
 pip install --upgrade pip
