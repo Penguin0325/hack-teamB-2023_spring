40facce0e53a4d21603d9d4d0343a061580fb233
Pengin <fierce0728birds@gmail.com>
2023-04-13 18:17:56 +0900

    yum

--- server/build_files.sh
+++ server/build_files.sh
@@ -1,11 +1,9 @@
 echo "BUILD START"
-cat /etc/*-release
-apt update
-apt-get update 
-apt-get clean 
-apt-get install -y libpq-dev gcc
-apt-get update
-apt-get -y install gcc libmariadb-dev mariadb-client
+cat /etc/*-release 
+yum clean 
+yum install -y libpq-dev gcc
+yum update
+yum -y install gcc libmariadb-dev mariadb-client
 pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
 echo "BUILD END"
