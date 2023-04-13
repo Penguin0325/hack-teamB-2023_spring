4fb0d58d4b8731c3aba012d8099fa5f8a899c7d3
Pengin <fierce0728birds@gmail.com>
2023-04-13 18:03:21 +0900

    build_sh修正

--- server/build_files.sh
+++ server/build_files.sh
@@ -1,4 +1,9 @@
 echo "BUILD START"
+apt-get update 
+apt-get clean 
+apt-get install -y libpq-dev gcc
+apt-get update
+apt-get -y install gcc libmariadb-dev mariadb-client
 pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
 echo "BUILD END"
