7da8e1bbffafe1c070fa432a831fcd7ed69954ec
Pengin <fierce0728birds@gmail.com>
2023-04-14 16:12:15 +0900

    pipテスト

--- server/build_files.sh
+++ server/build_files.sh
@@ -1,8 +1,9 @@
 echo "BUILD START"
 yum update
 yum clean all
+easy_install -U pip
 pip install --upgrade pip
-python -m pip install -U pip
+pip install -upgrade pip setuptools
 yum install MariaDB-server galera-4 MariaDB-client MariaDB-shared MariaDB-backup MariaDB-common git python3 python3-virtualenv make gcc gettext
 pip install -r requirements.txt
 pip install django-allauth
