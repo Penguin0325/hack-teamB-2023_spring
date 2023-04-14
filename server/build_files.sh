e22ecbd4af177362e9c02b3f6286558cd5fc4d1d
Pengin <fierce0728birds@gmail.com>
2023-04-14 16:07:26 +0900

    python

--- server/build_files.sh
+++ server/build_files.sh
@@ -1,10 +1,10 @@
 echo "BUILD START"
 yum update
 yum clean all
-python setup.py install
+pip install --upgrade pip
+python -m pip install -U pip
 yum install MariaDB-server galera-4 MariaDB-client MariaDB-shared MariaDB-backup MariaDB-common git python3 python3-virtualenv make gcc gettext
 pip install -r requirements.txt
-pip install --upgrade pip
 pip install django-allauth
 pip install django-widget-tweaks
 python3.9 manage.py collectstatic --noinput --clear
